fb390e25ff8866e6e237a9ba89a2d8648356e1d0
SantiagoOrozco <57675272+SantiagoOrozco@users.noreply.github.com>
2020-03-23 12:41:30 -0400

    front end interface with DOM Manipulation

--- form.js
+++ form.js
@@ -1,3 +1,44 @@
-let room1 = [];
+let room = [];
 
-document.getElementsByClassName
+document.getElementById("btn2").addEventListener("click", function(){
+    let name = document.getElementById("name").value
+    let last_name = document.getElementById("last_name").value
+    let phone = document.getElementById("phone").value
+    let email = document.getElementById("email").value
+    let famsize = document.getElementById("famsize").value
+
+    let fam = {
+        name: name,
+        last_name: last_name,
+        phone: phone,
+        email: email,
+        famsize: famsize
+    }
+
+    room.push(fam)
+
+    updateRoom();
+});
+
+function updateRoom(){
+
+    if (typeof(room[1]) === "object"){
+        return;
+    }
+
+    document.getElementById("box").innerHTML = ("")
+    let compList;
+    let list = document.createElement("ul")
+    for(let x=0; x<room.length; x++){
+        let keys = Object.keys(room[x])
+        for(let i=0; i<keys.length; i++){
+            let litag = document.createElement("li")
+            let value = (room[x][keys[i]])
+            litag.innerHTML = (value)
+            list.appendChild(litag)
+            compList = list
+        }
+        document.getElementById("box").appendChild(compList)
+        console.log(room)
+    }
+}
